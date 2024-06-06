eb12291a54ccc8ddb4e6d3832e7ce3b6e08447ae
Marie Claudine Ingenere <ingenerem@gmail.com>
2024-06-05 23:17:42 -0400

    Project initialization

---
+++ trust/src/test/java/pitech/trust/TrustApplicationTests.java
@@ -0,0 +1,13 @@
+package pitech.trust;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
